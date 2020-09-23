2f167544a5aa53d4cc2c09d35a03607b4a6c4311
Bartek Nietopiel <bartek94051@wp.pl>
2020-09-23 21:09:29 +0200

    articleSelector

--- js/script.js
+++ js/script.js
@@ -9,18 +9,30 @@ event.preventDefault();
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks)  {
-  activeLink.classList.remove('active');
-}
-
   /* [IN PROGRESS] add class 'active' to the clicked link */
 
-      const clickedElement = this;
+  const clickedElement = this;
+    activeLink.classList.Add('active');
+
     console.log('clickedElement:', clickedElement);
 
   /* [DONE] remove class 'active' from all articles links */
 
+  for(let activeLink of activeLinks)  {
+    activeLink.classList.remove('active');
+  }
+
   /* get 'href' attribute from the clicked link */
+
+  const articleSelector = href;
+    activeLink.classList.Add('href')
+      atrybut = element.getAttribute(nazwaAtrybutu)
+        var div1 = document.getElementById("href");
+        var a = div1.getAttribute("href");
+          alert(a);
+
+          console.log('artocleSelector');
+
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
