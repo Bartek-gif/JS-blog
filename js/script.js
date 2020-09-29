0d53543f9d196c95d5ab6dbfe1f8c22ceff9c439
Bartek Nietopiel <bartek94051@wp.pl>
2020-09-29 17:44:15 +0200

    Add innerHTML

--- js/script.js
+++ js/script.js
@@ -1,26 +1,19 @@
-const titleClickHandler = function(event) {
+const titleClickHandler = function(event){
 
 event.preventDefault();
 
 const clickedElement = this;
-console.log('Link was clicked!');
 
 /* [DONE] remove class 'active' from all article links  */
-
 const activeLinks = document.querySelectorAll('.titles a.active');
 
-  for(let activeLink of activeLinks)  {
-    activeLink.classList.remove('active');
-  }
-
-/* [IN PROGRESS] add class 'active' to the clicked link */
-
-  console.log('clickedElement (with plus): ' + clickedElement);
-
-  clickedElement.classList.add('active');
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
+/* [DONE] add class 'active' to the clicked link */
+ clickedElement.classList.add('active');
 
 /* [DONE] remove class 'active' from all articles */
-
 const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles) {
@@ -28,25 +21,13 @@ const activeArticles = document.querySelectorAll('.posts article.active');
 }
 
 /* get 'href' attribute from the clicked link */
-
 const articleSelector = clickedElement.getAttribute('href');
 
-  console.log(articleSelector);
-
 /* find the correct article using the selector (value of 'href' attribute) */
-
 const targetArticle = document.querySelector(articleSelector);
 
-  console.log(targetArticle);
-
 /* add class 'active' to the correct article */
-
-  targetArticle.classList.add('active');
-}
-
-  const links = document.querySelectorAll('.titles a');
-    for(let link of links)  {
-      link.addEventListener('click', titleClickHandler);
+targetArticle.classList.add('active');
 }
 
 const optArticleSelector = '.post',
@@ -56,9 +37,10 @@ const optArticleSelector = '.post',
 function generateTitleLinks() {
 
   /* remove contents of titleList */
+const titleList = document.querySelectory(optTitleSelector);
+titleList.innerHTML = '';
 
-  const titleList = document.querySelectory(optTitleSelector);
-    for (const article of articles) {
+
 
   /* for each article */
 
