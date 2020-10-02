e84ad82f42cb65f068c34cfbe2a398d1b32a0056
Bartek Nietopiel <bartek94051@wp.pl>
2020-10-02 20:19:54 +0200

    Add href.replace

--- js/script.js
+++ js/script.js
@@ -135,6 +135,7 @@ const titleClickHandler = function(event) {
 const href = clickedElement.getAttribute('href');
 
 /* make a new constant "tag" and extract tag from the "href" constant */
+  const tag = href.replace('#tag-', '');
 
 /* find all tag links with class active */
 
