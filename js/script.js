35af1b43571013ae95fcaa2e3eb071909968bd8a
Bartek Nietopiel <bartek94051@wp.pl>
2020-10-03 16:11:23 +0200

    change script.js

--- js/script.js
+++ js/script.js
@@ -23,10 +23,13 @@ const titleClickHandler = function(event){
 /* get 'href' attribute from the clicked link */
   const articleSelector = clickedElement.getAttribute('href');
 
-/* find the correct article using the selector (value of 'href' attribute) */
-  const targetArticle = document.querySelector(articleSelector);
+  console.log(artocleId);
 
-  console.log(targetArticle);
+/* find the correct article using the selector (value of 'href' attribute) */
+
+  const targetArticle = document.querySelector(clickedLink);
+  
+  console.log('Article: ', targetArticle);
 
 /* add class 'active' to the correct article */
   targetArticle.classList.add('active');
