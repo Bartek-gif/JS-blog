19d93f2715aca819c3b930e7b784b9940601cf34
Bartek Nietopiel <bartek94051@wp.pl>
2020-10-03 17:15:51 +0200

    change script.js

--- js/script.js
+++ js/script.js
@@ -43,34 +43,34 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
 
-function generateTitleLinks(customSelector = ''){
+  function generateTitleLinks(){
 
 /* remove contents of titleList */
-
-const titleList = document.querySelector(optTitleListSelector + customSelector);
+const titleList = document.querySelector(optTitleListSelector);
 titleList.innerHTML = '';
 
-/* for each article */
+const articles = document.querySelectorAll(optArticleSelector);
 
+/* for each article */
 let html = '';
 
 for(let article of articles){
-}
 
 /* get the article id */
- const articleId = article.getAttribute('id');
+  const articleId = article.getAttribute('id');
 
 /* find the title element */
 
 /* get the title from the title element */
- const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
 /* create HTML of the link */
- const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
 
 /* insert link into html variable */
- html = html + linkHTML;
- console.log(html);
+  html = html + linkHTML;
+  console.log(html);
 }
 
 titleList.innerHTML = html;
@@ -78,12 +78,12 @@ titleList.innerHTML = html;
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
- link.addEventListener('click', titleClickHandler);
-}
-
+link.addEventListener('click', titleClickHandler);
+    }
+  }
 generateTitleLinks();
 
-generateTags();
+generateTags()
 
   function tagClickHandler(event){
 /* prevent default action for this event */
