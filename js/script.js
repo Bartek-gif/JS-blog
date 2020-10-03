ddaed6ac549078ccab2ffcb76166914919d92d37
Bartek Nietopiel <bartek94051@wp.pl>
2020-10-03 16:07:01 +0200

    change script.js

--- js/script.js
+++ js/script.js
@@ -26,6 +26,8 @@ const titleClickHandler = function(event){
 /* find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(articleSelector);
 
+  console.log(targetArticle);
+
 /* add class 'active' to the correct article */
   targetArticle.classList.add('active');
 }
