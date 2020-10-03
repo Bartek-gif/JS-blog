709c1a1dedc8a12c194cd5166aee7cb64f813a73
Bartek Nietopiel <bartek94051@wp.pl>
2020-10-03 15:21:01 +0200

    change script.js

--- js/script.js
+++ js/script.js
@@ -88,30 +88,30 @@ generateTitleLinks();
   const titleList = article.querySelector(optArticleTagsSelector);
 
 /* make html variable with empty string */
-      let html ='';
+  let html ='';
 
 /* get tags from data-tags attribute */
-      const articleTags = article.getAttribute('data-tags');
-      console.log(articleTags);
+  const articleTags = article.getAttribute('data-tags');
+  console.log(articleTags);
 
 /* split tags into array */
-      const articleTagsArray = articleTags.split(' ');
+  const articleTagsArray = articleTags.split(' ');
 
 
 /* START LOOP: for each tag */
-      for(let tag of articleTagsArray) {
-        console.log(tag);
+  for(let tag of articleTagsArray) {
+  console.log(tag);
 
 /* generate HTML of the link */
-        const linkHTML = '<li><a href="#tag-' + tag  + '"> ' + tag + ' </a></li>';
-        console.log(linkHTML);
+  const linkHTML = '<li><a href="#tag-' + tag  + '"> ' + tag + ' </a></li>';
+  console.log(linkHTML);
 
 /* add generated code to html variable */
-        html = html + linkHTML;
-        console.log(html);
+html = html + linkHTML;
+console.log(html);
 
 /* END LOOP: for each tag */
-      }
+}
 
 /* insert HTML of all the links into the tags wrapper */
       titleList.innerHTML = html;
