f4f287164f73840aff92052c6e91fcf920c4eba5
Bartek Nietopiel <bartek94051@wp.pl>
2020-10-05 19:13:32 +0200

    Change function generateTags

--- js/script.js
+++ js/script.js
@@ -79,6 +79,10 @@
 generateTitleLinks();
 
   function generateTags() {
+
+/* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
+
 /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
 
@@ -100,6 +104,13 @@ generateTitleLinks();
   const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li> ';
 /* add generated code to html variable */
   html = html + linkHTML;
+
+/* [NEW] check if this link is NOT already in allTags */
+    if(allTags.indexOf(linkHTML) == -1){
+/* [NEW] add generated code to allTags array */
+    allTags.push(linkHTML);
+}
+
 /* END LOOP: for each tag */
 }
 /* insert HTML of all the links into the tags wrapper */
@@ -108,6 +119,13 @@ generateTitleLinks();
 /* END LOOP: for every article: */
   }
 }
+
+/* [NEW] find list of tags in right column */
+ const tagList = document.querySelector('.tags');
+
+ /* [NEW] add html from allTags to tagList */
+   tagList.innerHTML = allTags.join(' ');
+ }
 
 generateTags();
 
