42a18b65b20fb33eeb5be727a52a37c1ec6b5790
Bartek Nietopiel <bartek94051@wp.pl>
2020-10-06 20:09:42 +0200

    Add console.log in const tagLinkHTML

--- js/script.js
+++ js/script.js
@@ -149,6 +149,7 @@ let allTagsHTML = '';
 for(let tag in allTags){
 /* [NEW] generate code of a link and add it to allTagsHTML */
   const tagLinkHTML = '<li><a href="#tag-' + tag + '" class = "' + optCloudClassPrefix + calculateTagClass(allTags[tag], tagsParams) + '">' + tag + '</a></li>';
+  consolo.log('tagLinkHTML:', tagLinkHTML);
   allTagsHTML += tag + ' (' + allTags[tag] + ') ';
 }
 /* [NEW] END LOOP: for each tag in allTags: */
